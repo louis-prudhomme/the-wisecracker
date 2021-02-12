46735bed2b022eeb1a62dfe6bcc85ad184c81a4e
PrideTheDaemon <louis.prudhomme@hotmail.com>
2021-02-12 11:09:17 +0000

    Fixed grenade collisions Added flash effect

--- the-wisecracker/Assets/Scripts/GrenadeController.cs
+++ the-wisecracker/Assets/Scripts/GrenadeController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class GrenadeController : MonoBehaviour
 {
+    public GameObject flash;
+    private Rigidbody body;
+    private static GameObject player;
+
     private float baseVelocityForward = 12f;
     private float baseVelocityUp = 10f;
 
     private void Start()
     {
-        Rigidbody body = GetComponent<Rigidbody>();
+        body = GetComponent<Rigidbody>();
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
 
         body.AddForce(Utils.MousePosition(transform.position.y) - transform.position, 
             ForceMode.Impulse);
@@ -20,12 +26,23 @@ public class GrenadeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(player))
+            return;
+
+        GetComponentInChildren<MeshRenderer>().enabled = false;
+        body.isKinematic = true;
+
+        Instantiate(flash, transform.position, transform.rotation).SetActive(true);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         print("tamer");
     }
-
 }
